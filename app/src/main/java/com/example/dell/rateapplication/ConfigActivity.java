1af7eebae8206530da76ac769d3110817f1712b2
韩洁 <h1603459540@163.com>
2018-09-29 23:26:57 +0800

    最终版

--- app/src/main/java/com/example/dell/rateapplication/ConfigActivity.java
+++ app/src/main/java/com/example/dell/rateapplication/ConfigActivity.java
@@ -1,26 +1,58 @@
 package com.example.dell.rateapplication;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class ConfigActivity {
+public class ConfigActivity extends AppCompatActivity {
     private final String TAG = "ConfigActivity";
     EditText dollarText;
     EditText euroText;
     EditText wonText;
 
-    public void save(View view) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+        Intent intent = getIntent();
+        float dollar2 = intent.getFloatExtra("dollar_rate_key", 0.0f);
+        float euro2 = intent.getFloatExtra("euro_rate_key", 0.0f);
+        float won2 = intent.getFloatExtra("won_rate_key", 0.0f);
+        Log.i(TAG, "onCreate: dollar2=" + dollar2);
+        Log.i(TAG, "onCreate: euro2=" + euro2);
+        Log.i(TAG, "onCreate: won2=" + won2);
+
+        dollarText = (EditText) findViewById(R.id.dollar_rate);
+        euroText = (EditText) findViewById(R.id.euro_rate);
+        wonText = (EditText) findViewById(R.id.won_rate);
+        dollarText.setText(String.valueOf(dollar2));
+        euroText.setText(String.valueOf(euro2));
+        wonText.setText(String.valueOf(won2));
     }
-    public void openOne(View btn){
-        Intent config = new Intent(this,ConfigActivity.class);
-        config.putExtra("dollar_rate_key",dollarRate);
-        config.putExtra("euro_rate_key",euroRate);
-        config.putExtra("won_rate_key",wonRate);
-        Log.i(TAG, "openOne: dollarRate=" + dollarRate);
-        Log.i(TAG, "openOne: euroRate=" + euroRate);
-        Log.i(TAG, "openOne: wonRate=" + wonRate);
-        //startActivity(config);
-        startActivityForResult(config,1);
+
+    public void save(View btn) {
+        Log.i(TAG, "save: ");
+        float newDollar = Float.parseFloat(dollarText.getText().toString());
+        float newEuroi = Float.parseFloat(euroText.getText().toString());
+        float newWon = Float.parseFloat(wonText.getText().toString());
+        Log.i(TAG, "save: 获取到新的值");
+        Log.i(TAG, "save: newDollar=" + newDollar);
+        Log.i(TAG, "save: newEuroi=" + newEuroi);
+        Log.i(TAG, "save: newWon=" + newWon);
+
+        Intent intent = getIntent();
+        Bundle bdl = new Bundle();
+        bdl.putFloat("key_dollar", newDollar);
+        bdl.putFloat("key_euro", newEuroi);
+        bdl.putFloat("key_won", newWon);
+        intent.putExtras(bdl);
+        setResult(2, intent);
+
+        finish();
     }
+
 }
